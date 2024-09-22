b31ccb8b4fc30e38da619c12880fbb140b60015b
itsparasbisht <iamparasbisht@gmail.com>
2024-09-22 16:58:22 +0530

    added home page with list of blogs

---
+++ utils/formatDate.ts
@@ -0,0 +1,9 @@
+export function formatDate(timestamp: number): string {
+  const date = new Date(timestamp);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${day} ${month}, ${year}`;
+}
