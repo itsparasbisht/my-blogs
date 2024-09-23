d9bc31d933d94f652d4d16d8d9a367e7088568c0
itsparasbisht <iamparasbisht@gmail.com>
2024-09-23 20:49:56 +0530

    added social links

--- app/[title]/layout.tsx
+++ app/[title]/layout.tsx
@@ -8,9 +8,17 @@ export default function MainLayout({
   return (
     <>
       <main>{children}</main>
-      <footer>
+      <footer className="mt-12 mb-12">
+        <div className="text-[#D88B4E] flex gap-4 underline">
+          <Link href="https://www.linkedin.com/in/paras-bisht" target="_blank">
+            LinkedIn
+          </Link>
+          <Link href="https://github.com/itsparasbisht" target="_blank">
+            GitHub
+          </Link>
+        </div>
         <Link href="/">
-          <h2 className="text-4xl font-bold text-[#D88B4E] flex justify-center gap-2 mt-12 mb-12">
+          <h2 className="text-4xl font-bold text-[#D88B4E] mt-4">
             Blogs <span className="text-lg font-medium self-end">by Paras</span>
           </h2>
         </Link>
