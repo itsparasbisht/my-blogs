3a56986ae33b8f53bcfd18d0733ec5129161c64e
itsparasbisht <iamparasbisht@gmail.com>
2025-02-09 21:02:46 +0530

    removed footer from [id] layout

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function MainLayout({
   children,
 }: Readonly<{
@@ -8,24 +6,6 @@ export default function MainLayout({
   return (
     <>
       <main>{children}</main>
-      <footer className="mt-12 mb-12">
-        <div className="text-[#D88B4E] flex gap-4 underline">
-          <Link href="https://paras-bisht.netlify.app" target="_blank">
-            Website
-          </Link>
-          <Link href="https://www.linkedin.com/in/paras-bisht" target="_blank">
-            LinkedIn
-          </Link>
-          <Link href="https://github.com/itsparasbisht" target="_blank">
-            GitHub
-          </Link>
-        </div>
-        <Link href="/">
-          <h2 className="text-4xl font-bold text-[#D88B4E] mt-4">
-            Blogs <span className="text-lg font-medium self-end">by Paras</span>
-          </h2>
-        </Link>
-      </footer>
     </>
   );
 }
